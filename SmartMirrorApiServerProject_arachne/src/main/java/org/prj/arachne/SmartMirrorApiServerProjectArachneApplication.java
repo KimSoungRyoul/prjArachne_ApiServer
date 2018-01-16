69d8f8e8cb4f45d228fe6234da187a5c5f944832
KimSoungRyoul <KimSoungRyoul@gmail.com>
2018-01-17 01:27:52 +0900

    first commit ksr

---
+++ SmartMirrorApiServerProject_arachne/src/main/java/org/prj/arachne/SmartMirrorApiServerProjectArachneApplication.java
@@ -0,0 +1,12 @@
+package org.prj.arachne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMirrorApiServerProjectArachneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMirrorApiServerProjectArachneApplication.class, args);
+	}
+}
