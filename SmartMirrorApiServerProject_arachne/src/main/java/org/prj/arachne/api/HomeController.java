b9a1807f0171b0e37a20d1e355f5e56bb790349d
KimSoungRyoul <KimSoungRyoul@gmail.com>
2018-01-17 22:50:22 +0900

    song's supporting

---
+++ SmartMirrorApiServerProject_arachne/src/main/java/org/prj/arachne/api/HomeController.java
@@ -0,0 +1,17 @@
+package org.prj.arachne.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	
+	@GetMapping
+	public String index() {
+		
+		return "helloAPIServer";
+	}
+	
+	
+}
